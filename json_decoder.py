89a12ea08def5809804ffbb885946853258a9792
Onezoop <tyler.griffith.family@gmail.com>
2022-12-22 17:01:35 -0800

    New Card id: 0003

--- json_decoder.py
+++ json_decoder.py
@@ -18,7 +18,6 @@ def decode_card(id):
     card = cards[formatted_id]
     return Card(card["name"], card["cost"], card["power"], card["health"], card["tribe"], card["main"], card["aux"], 
                 card["mfx"], card["mfx_turn"], card["afx"], card["afx_turn"], card["efx"], card["efx_turn"])
-    
 
 # print(decode_card(1).name)
 # print(decode_card("0001").name)
