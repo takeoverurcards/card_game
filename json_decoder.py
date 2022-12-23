b83dcd5f418d5a271df99344f7b08a8246c3ec50
Onezoop <tyler.griffith.family@gmail.com>
2022-12-22 18:16:23 -0800

    Error creation when invalid id is used

--- json_decoder.py
+++ json_decoder.py
@@ -15,9 +15,13 @@ def decode_card(id):
         formatted_id += str(id)
     elif type(id) == str:
         formatted_id = id
+    else:
+        raise TypeError("INVALID ID FORMAT")
     card = cards[formatted_id]
     return Card(card["name"], card["cost"], card["power"], card["health"], card["tribe"], card["main"], card["aux"], 
                 card["mfx"], card["mfx_turn"], card["afx"], card["afx_turn"], card["efx"], card["efx_turn"])
 
 # print(decode_card(1).name)
 # print(decode_card("0001").name)
+
+decode_card(False)
