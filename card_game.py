ffebfe7fc716f26a2c40c23704d5b88df87dff07
takeoverurcards <121264822+takeoverurcards@users.noreply.github.com>
2022-12-23 23:21:48 -0500

    Update card_game.py

--- card_game.py
+++ card_game.py
@@ -6,34 +6,35 @@ from json_decoder import *
 table = Tk()
 table.resizable(width=0,height=0)
 table.configure(bg="black")
-# table.geometry("777x850")
-frame = Frame(table, pady=113, bg="black")
+frame = Frame(table, pady=60, bg="black")
+frame2 = Frame(table, bg="black")
 frame.pack()
+frame2.pack()
 
-slots = [0,1,2,3,4,5,7,9,10,11,13,14,16,17,18,20,22,23,24,25,26,27]
+slots = [1,2,3,4,5,7,9,10,11,16,17,18,20,22,23,24,25,26]
+g1 = g2 = d1 = d2 = []
+h1 = h2 = {}
 
 for i in slots:
     Label(frame, bg="grey", width=15, height=10, relief=SUNKEN).grid(row=floor(i/7), column=i%7)
 
-cards = []
+m11 = m12 = m13 = m21 = m22 = m23 = a11 = a12 = a13 = a14 = a15 = a21 = a22 = a23 = a24 = a25 = n1 = n2 = Label(frame, width=13, height=9, relief=RAISED, text="", wraplength=100, justify="center")
 
-for i in range(1,23):
-    cards.append(Label(frame, width=13, height=9, relief=RAISED, text=i, wraplength=100, justify="center"))
+m11_info = m12_info = m13_info = m21_info = m22_info = m23_info = a11_info = a12_info = a13_info = a14_info_info = a15_info = a21_info = a22_info = a23_info = a24_info = a25_info = n1_info = n2_info = ""
 
-[m11,m12,m13,m21,m22,m23,a11,a12,a13,a14,a15,a21,a22,a23,a24,a25,n1,n2,g1,g2,d1,d2] = cards
+cards = [m11,m12,m13,m21,m22,m23,a11,a12,a13,a14,a15,a21,a22,a23,a24,a25,n1,n2]
 
-cards = [m11,m12,m13,m21,m22,m23,a11,a12,a13,a14,a15,a21,a22,a23,a24,a25,n1,n2,g1,g2,d1,d2]
+cards_info = [m11_info, m12_info, m13_info, m21_info, m22_info, m23_info, a11_info, a12_info, a13_info, a14_info_info, a15_info, a21_info, a22_info, a23_info, a24_info, a25_info, n1_info, n2_info]    
 
-for i in range(22):
+for i in range(len(slots)):
     cards[i].grid(row=floor(slots[i]/7), column=slots[i]%7)
 
-m11.config(text=decode_card("0001").name)
-m12.config(text=decode_card("0002").name)
-m13.config(text=decode_card("0003").name)
+for i in json.load(open("card_master_list.json", "r"))["Cards"]:
+    h1[i] = Label(frame2, width=13, height=4, relief=RAISED, text=decode_card(i).name, wraplength=100, justify="center")
 
 def info(x):
     global card_info
-    card_info = Label(justify="left",text=decode_card(x).name+"\ncost: "+
+    card_info = Label(table, justify="left",text=decode_card(x).name+"\ncost: "+
           str(decode_card(x).cost)+"\npower: "+
           str(decode_card(x).power)+"\nhealth: "+
           str(decode_card(x).health)+"\ntribe: "+
@@ -47,14 +48,29 @@ def info(x):
           decode_card(x).efx+"\nefx_turn: "+
           str(decode_card(x).efx_turn))
 
-def show():
+def show_info():
     card_info.place(x=table.winfo_pointerx() - table.winfo_rootx(),y=table.winfo_pointery() - table.winfo_rooty())
-def hide():
+
+def hide_info():
     card_info.place_forget()
 
+def update_field():
+    for i in range(len(cards_info)):
+        if cards_info[i]:
+            cards[i].grid(row=floor(slots[i]/7), column=slots[i]%7)
+        else:
+            cards[i].grid_forget()
+    table.update_idletasks()
+
+update_field()
+
 info("0001")
-m11.bind("<Button-1>", lambda i:[hide(),info("0001"),show()])
-m12.bind("<Button-1>", lambda i:[hide(),info("0002"),show()])
-m13.bind("<Button-1>", lambda i:[hide(),info("0003"),show()])
-frame.bind("<Button-1>", lambda i:hide())
+for i in range(len(cards)):
+    cards[i].bind("<Button-1>", lambda z:[hide_info(),info(cards_info[i]),show_info()])
+for i in range(len(h1)):
+    h1[list(h1)[i]].grid(row=0,column=i)
+    h1[list(h1)[i]].bind("<Button-1>", lambda z:[hide_info(),info(list(h1)[i]),show_info()])
+    update_field()
+frame.bind("<Button-1>", lambda z:hide_info())
+
 table.mainloop()
