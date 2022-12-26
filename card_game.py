626950028eb0b7744ed3807490a04c5486275627
takeoverurcards <121264822+takeoverurcards@users.noreply.github.com>
2022-12-25 23:28:21 -0500

    New functions; ability to play cards added
    select_card() and play_card() functions, selected_card variable added

--- card_game.py
+++ card_game.py
@@ -13,13 +13,15 @@ frame.pack()
 frame2.pack()
 
 slots = [1,2,3,4,5,7,9,10,11,16,17,18,20,22,23,24,25,26]
-g1 = g2 = d1 = d2 = []
-h1 = h2 = {}
+g1 = []
+g2 = []
+d1 = []
+d2 = []
+h1 = {}
+h2 = {}
+card_slots = {}
 
-for i in slots:
-    Label(frame, bg="grey", width=15, height=10, relief=SUNKEN).grid(row=floor(i/7), column=i%7)
-
-m11 = m12 = m13 = m21 = m22 = m23 = a11 = a12 = a13 = a14 = a15 = a21 = a22 = a23 = a24 = a25 = n1 = n2 = Label(frame, width=13, height=9, relief=RAISED, text="", wraplength=100, justify="center")
+m11 = m12 = m13 = m21 = m22 = m23 = a11 = a12 = a13 = a14 = a15 = a21 = a22 = a23 = a24 = a25 = n1 = n2 = ""
 
 m11_info = m12_info = m13_info = m21_info = m22_info = m23_info = a11_info = a12_info = a13_info = a14_info_info = a15_info = a21_info = a22_info = a23_info = a24_info = a25_info = n1_info = n2_info = ""
 
@@ -27,21 +29,48 @@ cards = [m11,m12,m13,m21,m22,m23,a11,a12,a13,a14,a15,a21,a22,a23,a24,a25,n1,n2]
 
 cards_info = [m11_info, m12_info, m13_info, m21_info, m22_info, m23_info, a11_info, a12_info, a13_info, a14_info_info, a15_info, a21_info, a22_info, a23_info, a24_info, a25_info, n1_info, n2_info]    
 
+for i in range(len(slots)):
+    cards[i] = Label(frame, name="slot_"+str(i).zfill(2), width=15, height=10, relief=SUNKEN, wraplength=100, justify="center")
+    cards[i].grid(row=floor(slots[i]/7), column=slots[i]%7)
+    cards[i].bind("<Button-1>", lambda z:play_card(z.widget))
+
 for i in json.load(open("card_master_list.json", "r"))["Cards"]:
     d1.append(i)
-    print(i)
 
 shuffle(d1)
 
-draw_count = 0
+draw_count = play_var = 0
+selected_card = ""
 
 def draw(x):
-    h1[d1[x]] = Label(frame2, name=d1[x], width=13, height=4, relief=RAISED, text=decode_card(d1[x]).name, wraplength=100, justify="center")
     global draw_count
+    h1[str(draw_count).zfill(3)+"_"+d1[x]] = Label(frame2, name="1_"+str(draw_count).zfill(3)+"_"+d1[x], width=13, height=4, relief=RAISED, text=decode_card(d1[x]).name, wraplength=90, justify="center")
     h1[list(h1)[-1]].grid(row=0,column=draw_count)
     draw_count += 1
+    h1[list(h1)[-1]].bind("<Button-1>", lambda z:select_card(z.widget))
     h1[list(h1)[-1]].bind("<Button-3>", lambda z:[hide_info(),info(str(z.widget)[-4:]),show_info()])
     d1.pop(x)
+
+def select_card(x):
+    global selected_card
+    global play_var
+    selected_card = x
+    x.config(relief=RIDGE)
+    play_var = 1
+    hide_info()
+
+def play_card(x):
+    global selected_card
+    global play_var
+    if play_var:
+        if decode_card(str(selected_card)[-4:]).tribe.lower() != "item":
+            cards_info[int(str(x)[-2:])] = str(selected_card)[-4:]
+            h1[str(selected_card)[-8:]].grid_forget()
+            g1.append(str(selected_card)[-4:])
+            del h1[str(selected_card)[-8:]]
+            update_field()
+            play_var = 0
+            print(cards_info)
 
 def info(x):
     global card_info
@@ -68,19 +97,17 @@ def hide_info():
 def update_field():
     for i in range(len(cards_info)):
         if cards_info[i]:
-            cards[i].grid(row=floor(slots[i]/7), column=slots[i]%7)
+            cards[i].config(bg="white", relief=RAISED, text=decode_card(cards_info[i]).name)
         else:
-            cards[i].grid_forget()
+            cards[i].config(bg="grey", relief=SUNKEN)
     table.update_idletasks()
 
 update_field()
 
 info("0001")
 table.bind('<Return>',lambda z:draw(0))
-#for i in range(len(cards)):
-#    cards[i].bind("<Button-3>", lambda z:[hide_info(),info(cards_info[i]),show_info()])
-for i in range(len(h1)):
-    h1[list(h1)[i]].grid(row=0,column=i)
+for i in range(len(cards)):
+    cards[i].bind("<Button-3>", lambda z:[hide_info(),info(cards_info[i]),show_info()])
 frame.bind("<Button-3>", lambda z:hide_info())
 
 table.mainloop()
